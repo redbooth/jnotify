90f565968e21fb8bd8d392a3765f8829578cdbe7
Hugues Bruant <hugues@aerofs.com>
2014-04-16 14:23:27 -0700

    Fixes and cleanups
    Avoid methods that work with Java's stupid modified UTF-8
    encoding. The real world works with regular UTF-8 and Java
    would choke on that.
    
    Fix incorrect use of DeleteLocalRef on a GlobalRef that
    caused memory corruption on OSX when the kernel dropped
    notifications.
    
    Send full FSEvents flags.
    
    Trim down Java code (remove all *Adapter classes that
    we don't actually use)

--- java/net/contentobjects/jnotify/macosx/JNotify_macosx.java
+++ java/net/contentobjects/jnotify/macosx/JNotify_macosx.java
@@ -2,8 +2,34 @@ package net.contentobjects.jnotify.macosx;
 
 import net.contentobjects.jnotify.JNotifyException;
 
+import static net.contentobjects.jnotify.Util.CHARSET_UTF;
+
 public class JNotify_macosx
 {
+    public static final int MUST_SCAN_SUBDIRS   = 0x00000001;
+    public static final int USER_DROPPED        = 0x00000002;
+    public static final int KERNEL_DROPPED      = 0x00000004;
+    public static final int EVENT_IDS_WRAPPED   = 0x00000008;
+    public static final int HISTORY_DONE        = 0x00000010;
+    public static final int ROOT_CHANGED        = 0x00000020;
+    public static final int MOUNT               = 0x00000040;
+    public static final int UNMOUNT             = 0x00000080;
+
+    // NB: the following flags are only valid for per-file notifications
+    // TODO: enable per-file notifications on recent versions of OSX (10.7+)
+    public static final int ITEM_CREATED         = 0x00000100;
+    public static final int ITEM_REMOVED         = 0x00000200;
+    public static final int ITEM_INODE_META_MOD  = 0x00000400;
+    public static final int ITEM_RENAMED         = 0x00000800;
+    public static final int ITEM_MODIFIED        = 0x00001000;
+    public static final int ITEM_FINDER_INFO_MOD = 0x00002000;
+    public static final int ITEM_CHANGE_OWNER    = 0x00004000;
+    public static final int ITEM_XATTR_MOD       = 0x00008000;
+    public static final int ITEM_IS_FILE         = 0x00010000;
+    public static final int ITEM_IS_DIR          = 0x00020000;
+    public static final int ITEM_IS_SYMLINK      = 0x00040000;
+
+
 	private static Object initCondition = new Object();
 	private static Object countLock = new Object();
 	private static int watches = 0;
@@ -91,11 +117,11 @@ public class JNotify_macosx
 		return removed;
 	}
 
-	public static void callbackProcessEvent(int wd, String rootPath, String filePath, boolean recurse)
+	public static void callbackProcessEvent(int wd, byte[] filePath, int flags)
 	{
 		if (_eventListener != null)
 		{
-			_eventListener.notifyChange(wd, rootPath, filePath, recurse);
+			_eventListener.notifyChange(wd, new String(filePath, CHARSET_UTF), flags);
 		}
 	}
 
